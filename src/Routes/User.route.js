22471996bf8829e70cf2a5292ed9f1c895c95eea
Salbeen <wizsalbeen69@gmail.com>
2026-09-19 03:53:38 -0400

    feat: add JWT verification to refresh token route and implement profile image upload route

--- src/Routes/User.route.js
+++ src/Routes/User.route.js
@@ -2,11 +2,14 @@ import express from "express";
 import { Signup } from "../Controllers/User.controller.js";
 import { Login } from "../Controllers/User.controller.js";
 import { refreshToken } from "../Controllers/User.controller.js";
+import { VerifyJWT } from "../Middlewares/auth.middleware.js";
+import { uploadProfileImage } from "../Controllers/User.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.post("/refresh", refreshToken);
+router.post("/refresh", VerifyJWT, refreshToken);
+router.post("/upload-profile-image", VerifyJWT, uploadProfileImage);
 
 export default router;
